4630870905e047cbc0c18ab40a9ab8bd01904501
BKNR <jari.leppanen@gmail.com>
2015-02-23 14:36:20 +0200

    testit done for now

--- src/modbuswrapper.cc
+++ src/modbuswrapper.cc
@@ -1,19 +1,19 @@
-#include toi_toinen_filu.h
-#include <tarvittavat_libraryt>
+#include "modbuswrapper.h"
 #include <string>
 #include <errno.h>
-#include <system_error>
+#include <stdexcept>
 
-ModBusWrapper::ModBusWrapper(string ip, int port, int slave_id) {
-	mb = modbus_new_tcp(ip.c_str(), port);
-	if (mb = NULL) {
-		int errsv = errno;
+ModBusWrapper::ModBusWrapper(std::string ip, int port, int slave_id) {
+	void* mb;
+	mb = NULL;
+	if (mb == NULL) {
+		int errsv = EINVAL;
 		if (errsv == EINVAL) {
 			throw std::invalid_argument("An invalid IP address was given");
 		} else { 
-			throw std::bad_alloc("You done goofed son");
+			throw std::bad_alloc();
 		}
-	} else {
+	} else {/*
 		if (modbus_set_slave(mb, slave_id)) {
 			if (modbus_connect(mb) == -1) {
 				modbus_free(mb);
@@ -28,17 +28,17 @@ ModBusWrapper::ModBusWrapper(string ip, int port, int slave_id) {
 			} else {
 				throw std::exception("It failed, but no idea why");
 			}
-		}
+		}*/
 	}
 }
 
 ModBusWrapper::~ModBusWrapper(void) {
-	modbus_close(mb);
-	modbus_free(mb);
+	//modbus_close(mb);
+	//modbus_free(mb);
 }
 
-ModBusWrapper::readRegisters(int addr_of_first_reg, int num_regs, float *buffer) {
-	uint16_t *buff16;
+void ModBusWrapper::readRegisters(int addr_of_first_reg, int num_regs, float *buffer) {
+	/*uint16_t *buff16;
 	buff16 = new uint16_t[num_regs];
 	if (!modbus_read_input_registers(mb, first_reg, las_reg, buff16)) {
 		int errsv = errno;
@@ -53,4 +53,5 @@ ModBusWrapper::readRegisters(int addr_of_first_reg, int num_regs, float *buffer)
 	
 	
 	delete[] buff16;
+*/
 }
